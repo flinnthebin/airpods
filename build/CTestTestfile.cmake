8cc7ee6d9b80c9b7dbe1c982b81f325affc2cafb
Chris Flinn <christopher.flinn@gmail.com>
2024-06-15 16:03:45 +1000

    more changes

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/archer/git/ble
-# Build directory: /home/archer/git/ble/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ble_test "/home/archer/git/ble/build/ble_test")
-set_tests_properties(ble_test PROPERTIES  _BACKTRACE_TRIPLES "/home/archer/git/ble/CMakeLists.txt;69;add_test;/home/archer/git/ble/CMakeLists.txt;0;")
